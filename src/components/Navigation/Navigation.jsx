1191cb157006416dc337d2f6878bc683bfaf99a6
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-09 15:55:43 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
+    return (
+        <div className='flex flex-col gap-2 bg-[#202020]'>
+            <Link to={"/"} >Home </Link>
+            <Link to={"/about"} >About </Link>
+            <Link to={"/projects"} >Projects </Link>
+            <Link to={"/certificates"} >Certificates </Link>
+            <Link to={"/contacts"} >Contacts </Link>
+        </div>
+    )
 }
