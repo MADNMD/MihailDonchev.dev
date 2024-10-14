4aadc16311560a765fda93b4424908410969f260
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-14 15:10:38 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/styles.css';
 
 export const Navigation = () => {
     return (
-            <nav className="flex-grow flex items-center justify-center">
-                <ul className='flex flex-col gap-4'>
-                    <li>
-                        <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4 hover:bg-red-600">About </Link>
-                    </li>
-                    <li>
-                        <Link to={"/projects"} className="text-lg text-decoration-none text-navigation-text py-2 px-4 hover:bg-red-600" >Projects </Link>
-                    </li>
-                    <li>
-                        <Link to={"/certificates"} className="text-lg text-decoration-none text-navigation-text py-2 px-4 hover:bg-red-600" >Certificates </Link>
-                    </li>
-                    <li>
-                        <Link to={"/contacts"} className="text-lg text-decoration-none text-navigation-text py-2 px-4 hover:bg-red-600" >Contacts </Link>
-                    </li>
-                </ul>
-            </nav>
+        <nav className="flex-grow flex items-center justify-center">
+            <ul className='flex flex-col gap-4'>
+                <li className='text-center'>
+                    <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4">ABOUT </Link>
+                </li>
+                <li className='text-center'>
+                    <Link to={"/projects"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >PROJECTS </Link>
+                </li>
+                <li className='text-center'>
+                    <Link to={"/certificates"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >CERTIFICATES </Link>
+                </li>
+                <li className='text-center'>
+                    <Link to={"/contacts"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >CONTACTS </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
