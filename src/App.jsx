3642afbb8c49969292bf402060ab1fa88fdbda4a
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-14 14:22:11 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './global.css';
+import './global.css'; 
 
 import { Navigation } from "./components/Navigation/Navigation";
 import { Logo } from './components/Logo/Logo';
 import { Footer } from './components/Footer/Footer';
 
-
 function App() {
-
     return (
         <div className='flex'>
             <div className="flex flex-col justify-between bg-navigation-bg w-46 h-screen">
@@ -18,4 +16,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
