64d3fa143643978638c48fa0ff2caf6a3368e330
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-15 13:40:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import './global.css'; 
+import './global.css';
 
 import { Navigation } from "./components/Navigation/Navigation";
 import { Logo } from './components/Logo/Logo';
 import { Footer } from './components/Footer/Footer';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
     return (
         <div className='flex'>
-            <div className="flex flex-col justify-between bg-navigation-bg w-48 h-screen">
-                <Logo />
-                <Navigation />
-                <Footer />
+            <div className='flex'>
+                <div className="flex flex-col justify-between bg-navigation-bg w-48 h-screen">
+                    <Logo />
+                    <Navigation />
+                    <Footer />
+                </div>
+            </div>
+            <div className='bg-body-bg w-screen'>
+                <Projects />
             </div>
         </div>
     )
