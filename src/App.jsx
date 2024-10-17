4d058c591cfaf7f720344500a538f146b60a0d04
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-17 12:01:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Logo } from './components/Logo/Logo';
 import { Footer } from './components/Footer/Footer';
 import { Projects } from './components/Projects/Projects';
 import { Certificates } from './components/Certificates/Certificates';
+import { Contacts } from './components/Contacts/Contacts';
 
 function App() {
     return (
@@ -21,6 +22,7 @@ function App() {
                 <Routes>
                     <Route path='/projects' element={<Projects />} />
                     <Route path='/certificates' element={<Certificates />} />
+                    <Route path='/contacts' element={<Contacts />} />
                 </Routes>
             </div>
         </div>
