6cd4ae959ac37f0b7c257f0b50fa4ecbb5520788
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-18 11:13:51 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ export const Footer = () => {
     return (
         <div className='bg-navigation-bg p-4 text-center'>
             <p className="inline-block mx-2">
-                <Link to={'https://www.linkedin.com/in/mihail-donchev-6a400025a/'}><FaGithub className="text-navigation-text text-2xl" /></Link>
+                <Link to={'https://www.linkedin.com/in/mihail-donchev-6a400025a/'} target='_blank'><FaGithub className="text-navigation-text text-2xl" /></Link>
             </p>
             <p className="inline-block mx-2">
-                <Link to={'https://github.com/MADNMD'}><FaLinkedin className="text-navigation-text text-2xl" /></Link>
+                <Link to={'https://github.com/MADNMD'} target='_blank'><FaLinkedin className="text-navigation-text text-2xl" /></Link>
             </p>
         </div>
     )
