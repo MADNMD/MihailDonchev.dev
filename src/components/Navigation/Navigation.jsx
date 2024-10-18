b49ddcb5ce28bc0a5b5a912806163a00c0f6fabf
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-18 11:13:54 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export const Navigation = () => {
         <nav className="flex-grow flex items-center justify-center">
             <ul className='flex flex-col gap-4'>
                 <li className='text-center'>
-                    <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4">ABOUT </Link>
+                    <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4">ABOUT ME</Link>
                 </li>
                 <li className='text-center'>
                     <Link to={"/projects"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >PROJECTS </Link>
