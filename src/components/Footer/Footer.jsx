5f6b8cca03fd1efd67248928f8ced0c8d04b8b9e
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-21 11:14:03 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import WorldFlag from 'react-world-flags';
+import { AppContext } from "../../contexts/AppContext.jsx";
 
 export const Footer = () => {
+
+    const { handleChangeLanguage, text, currentLanguage } = useContext(AppContext);
+
     return (
         <div className='bg-navigation-bg p-4 text-center'>
-            {/* <p className="inline-block mx-2">
-                <Link to={'https://www.linkedin.com/in/mihail-donchev-6a400025a/'} target='_blank'><FaGithub className="text-navigation-text text-2xl" /></Link>
-            </p>
-            <p className="inline-block mx-2">
-                <Link to={'https://github.com/MADNMD'} target='_blank'><FaLinkedin className="text-navigation-text text-2xl" /></Link>
-            </p> */}
-            <p className="inline-block mx-2">
+            <button className="inline-block mx-2" onClick={() => handleChangeLanguage('en')}>
+                {currentLanguage === 'en'}
                 <WorldFlag code="GB" className="w-8 h-8 inline-block" title="English" />
-            </p>
-            <p className="inline-block mx-2">
+            </button>
+            <button className="inline-block mx-2" onClick={() => handleChangeLanguage('bg')}>
+                {currentLanguage === 'bg'}
                 <WorldFlag code="BG" className="w-8 h-8 inline-block" title="Bulgarian" />
-            </p>
+            </button>
         </div>
     )
 }
