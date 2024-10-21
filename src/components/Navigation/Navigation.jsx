cdb4a6674dd8d488898766fa37926fe938a419d1
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-21 11:14:09 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/styles.css';
 
+import { AppContext } from '../../contexts/AppContext';
+
 export const Navigation = () => {
+
+    const { text } = useContext(AppContext);
+
     return (
         <nav className="flex-grow flex items-center justify-center">
             <ul className='flex flex-col gap-4'>
                 <li className='text-center'>
-                    <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4">ABOUT ME</Link>
+                    <Link to={"/about"} className="text-lg text-decoration-none text-navigation-text py-2 px-4">{text('navigation.about')}</Link>
                 </li>
                 <li className='text-center'>
-                    <Link to={"/projects"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >PROJECTS </Link>
+                    <Link to={"/projects"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >{text('navigation.projects')}</Link>
                 </li>
                 <li className='text-center'>
-                    <Link to={"/certificates"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >CERTIFICATES </Link>
+                    <Link to={"/certificates"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >{text('navigation.certificates')}</Link>
                 </li>
                 <li className='text-center'>
-                    <Link to={"/contacts"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >CONTACT МЕ </Link>
+                    <Link to={"/contacts"} className="text-lg text-decoration-none text-navigation-text py-2 px-4" >{text('navigation.contacts')}</Link>
                 </li>
             </ul>
         </nav>
