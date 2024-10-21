05e54a7577ad51fb040994b5f9692758628071ff
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-21 14:59:45 +0300

    Update Certificates.jsx

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { AppContext } from '../../contexts/AppContext';
 
 export const Certificates = () => {
+
+    const { text } = useContext(AppContext);
+
     return (
         <motion.div className='flex flex-col items-center justify-center'
             initial={{ opacity: 0, y: 20 }}
@@ -20,7 +24,7 @@ export const Certificates = () => {
                         <img src="/MySQL.jpeg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>One of the most widely used database management systems in the development of modern applications. Relational databases, basic functionalities such as working with tables, grouping functions, transactions, procedures, etc.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.mysql')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -37,7 +41,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -47,7 +51,7 @@ export const Certificates = () => {
                         <img src="/ReactJS.jpeg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>Knowledge and practical skills for SPA development using ReactJS technology. JSX syntax working with requests and promises, splitting an application into components, form routing and validation, different ways to style components using techniques like CSS modules and Styled Components. React Hooks, Context API.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.react')}</p>
                     </div>
                     <div className='flex justify-evenly gap-20'>
                         <Link to={'https://softuni.bg/certificates/details/197803/9cd256fd'} target='_blank' className='
@@ -63,7 +67,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -73,7 +77,7 @@ export const Certificates = () => {
                         <img src="/HTML-CSS.jpeg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>In this course, I learned the basics of HTML5 and CSS3 web technologies. I learned the basic HTML tags, paragraphs, lists, tables, forms, controls and semantic HTML tags. Emphasis is on basic CSS selectors and styles, working with classes, presentation rules, positioning and layout rules in CSS. The course emphasizes hands-on exercises with HTML and CSS, working on real web page development projects, implementation of site fragments and creating complete modern websites with a good look, modern structure and mobile support.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.htmlCss')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -90,7 +94,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -100,7 +104,7 @@ export const Certificates = () => {
                         <img src="/Angular.jpeg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>The "Angular" course provides knowledge and practical skills for building client applications (Single Page Applications) with Angular technology, a working framework created by Google and one of the most popular at the moment. You will learn what TypeScript is, basic syntax and concepts, and what more it offers over JavaScript, such as conveniences for development in the browser and on mobile platforms. Learn what Single Page Application (SPA) is, architectural patterns for SPA applications, components, directives and their implementation in Angular. Views and templates for data visualization (templates), models, components, data binding, working with many views, observables, routing, extracting data from a REST service and dependency injection are studied. DI</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.angular')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -117,7 +121,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -127,7 +131,7 @@ export const Certificates = () => {
                         <img src="/beck-end.jpeg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>In the "JS Back-End" course, you will learn how to develop server-side JavaScript applications on the Node.js platform using Express.js as a framework. Also during the training you will learn how to create a server with Node.js, learn the server-client architecture and how to make easy and fast data-driven web applications with Node.js, Express.js and MongoDB using modern techniques and tools. One of the most popular non-relational databases, MongoDB, will be covered, along with the Mongoose library for retrieving data from it. At the end of the course, a complete REST API will be built from scratch using Express.js and MongoDB.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.jsBackEnd')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -144,7 +148,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -154,7 +158,7 @@ export const Certificates = () => {
                         <img src="/applications.jpg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>In the course "JS Applications" you will learn what HTTP requests are and how to use them. You will learn about REST Services, what a Baas (backend as a service) is and how to work with it, what asynchronous code means (Promises, using async/await), what Templating and Routing are. During the course, you will create a Single Page Application, using the techniques learned from previous lectures, on which you will be assessed. You will understand what is the architecture of an application and how to properly structure your applications.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.jsApp')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -171,7 +175,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -181,7 +185,7 @@ export const Certificates = () => {
                         <img src="/advanced.jpg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>In the "JS Advanced" course, you will get a deep knowledge of the JavaScript language, such as the basics of syntax, working with arrays, matrices, objects, classes and writing functions. You will study more complex concepts like function context, explicit binding, event loop. The course will develop your algorithmic thinking. After successful completion of this course, you will be able to work with the DOM tree, do manipulations on it and work with "events". Functional and OOP approaches to JavaScript programming will be explored, studying concepts such as inheritance, object composition, and prototype chaining.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.jsAdvanced')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -198,7 +202,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -208,7 +212,7 @@ export const Certificates = () => {
                         <img src="/fundamentals.jpg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>The course "Programming Fundamentals" examines fundamental concepts in programming, which are the basis for effective and quality training in the professional modules. Learners build on the knowledge gained in the Programming Basics course and begin to use more complex programming concepts and structures such as: arrays, objects and classes. Additional technologies needed by every software developer are studied, such as HTML and CSS, HTTP, bit operations, and more. The fundamentals of web development are covered, as well as one of the most common technologies and tools with which the course successfully builds students' ideas about the upcoming professional modules and the knowledge they will gain from them to become successful software developers.</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.fundamentals')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -225,7 +229,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
 
@@ -235,7 +239,7 @@ export const Certificates = () => {
                         <img src="/basics.jpg" alt="Online supermarket" className='w-full h-max rounded-lg ' />
                     </div>
                     <div className='flex-grow'>
-                        <p className='text-white mb-4 mt-4'>The "Programming Basics" course provides the initial programming skills necessary for all technology majors at Software University. This includes writing entry-level programming code (basic coding skills), working with a development environment (IDE), using variables and data, operators and expressions, working with the console (reading input and printing results), using of conditional constructions (if, if-else) and loops (for, while).</p>
+                        <p className='text-white mb-4 mt-4'>{text('certificates.basic')}</p>
 
                     </div>
                     <div className='flex justify-evenly gap-20'>
@@ -252,7 +256,7 @@ export const Certificates = () => {
                         transition-colors 
                         duration-300 
                         ease-in-out
-                        hover:bg-hover'>To Do Certificate</Link>
+                        hover:bg-hover'>{text('certificates.btn')}</Link>
                     </div>
                 </div>
             </div>
