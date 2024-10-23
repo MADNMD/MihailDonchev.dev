01a463fb998878a73f48e75d7aeebb0794bc08c1
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-23 15:30:45 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Logo = () => {
+export const Logo = ({closeMenu}) => {
     return (
         <div className='flex flex-col bg-black p-4 text-center'>
-            <Link to={'/'}>
+            <Link to={'/'} onClick={closeMenu}>
                 <div className='flex items-center justify-center'><img src='/m-logo.png' alt="" className='w-24 h-24' /></div>
                 <p className='text-navigation-text font-custom-1'>MIHAIL</p>
                 <p className='text-navigation-text font-custom-2'>Junior web developer</p>
