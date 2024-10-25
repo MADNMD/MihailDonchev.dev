80723c876045ac39e5419559d7450d8362ba778c
Mihail Donchev <mihaildonchev88@abv.bg>
2024-10-25 11:33:40 +0300

    Update Certificates.jsx

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -16,7 +16,15 @@ export const Certificates = () => {
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1 }}>{text('certificates.header')}</motion.h1>
-            <div className='grid grid-cols-3 gap-4 mt-8 mr-4 ml-4'>
+            <div className={`
+                grid gap-4 mt-8 mr-4 ml-4
+                xxs:grid-cols-1
+                xs:grid-cols-1
+                sm:grid-cols-1
+                md:grid-cols-2
+                lg:grid-cols-2
+                xl:grid-cols-3
+                `}>
 
                 <div className='bg-gray-700 shadow-lg rounded-lg p-4 flex flex-col'>
                     <div>
